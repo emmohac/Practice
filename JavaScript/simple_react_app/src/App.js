55a468e9b4d5eceb697a93c48f660c29c7bf7cdf
Huy Minh Tran <emmohac.tran@yahoo.com>
2019-09-12 10:20:07 -0700

    Adding new lesson

--- JavaScript/simple_react_app/src/App.js
+++ JavaScript/simple_react_app/src/App.js
@@ -1,60 +1,33 @@
-import React from "react";
-import Titles from "./component/titles";
-import Form from "./component/form";
-import Weather from "./component/weather";
+import React, { Component } from "react";
+import { throwStatement } from "@babel/types";
 
-const API_KEY = "0784a2c59aeebf19423679b0284eb6a4"; //api key
-class App extends React.Component {
-  state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined
-  };
-  getWeather = async e => {
-    e.preventDefault();
+const API_KEY = "84396c2022ea41aa83f62bff85b95619";
+const URL = "https://api.weatherbit.io/v2.0/current?city=";
 
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    if (city && country) {
-      const API_CALL = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
-      );
-      const response = await API_CALL.json();
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			weather: "",
+			theCity: "",
+			theState: "",
+			theCountry: ""
+		};
+	}
 
-      this.setState({
-        temperature: response.main.temp,
-        city: response.name,
-        country: response.sys.country,
-        humidity: response.main.humidity,
-        description: response.weather[0].description,
-        error: ""
-      });
-      console.log(response);
-    } else {
-      this.setState({
-        error: "Please enter the value in the field"
-      });
-    }
-  };
-  render() {
-    return (
-      <div>
-        <Titles />
-        <Form loadWeather={this.getWeather} />
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-        />
-      </div>
-    );
-  }
+	changeCity = e => {
+		this.setState({ city: e.target.value });
+	};
+
+	render() {
+		return (
+			<div>
+				<input id="city" onChange={this.changeCity}>
+					City name
+				</input>
+			</div>
+		);
+	}
 }
 
 export default App;
